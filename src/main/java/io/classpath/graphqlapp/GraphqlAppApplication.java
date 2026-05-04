805694988f780f47e3ba5ff4ec607d36d3f532bf
pradeep <pradeep.kumar44@gmail.com>
2026-05-04 10:50:18 +0530

    first commit

---
+++ src/main/java/io/classpath/graphqlapp/GraphqlAppApplication.java
@@ -0,0 +1,13 @@
+package io.classpath.graphqlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlAppApplication.class, args);
+    }
+
+}
